7c6609c876c7b611e5a40324d2a40766233916b3
AndyMMunro <agovitadventures@gmail.com>
2020-07-29 06:50:24 -0700

    basics of html and css with minimal java FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function start() {
+    console.log('started')
+    startButton.classList.add('hide')
+}
+
+function nextQuestion() {
+
+}
+
+function previousQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
