88f0fe5df9908b4f7e7dcba8224d520746dcd023
AndyMMunro <agovitadventures@gmail.com>
2020-08-04 23:29:12 -0700

    placed timer in start game and hide the question container

--- script.js
+++ script.js
@@ -1,117 +1,140 @@
-// Timer variables
-var timerEl = document.getElementById("countdowntimer");
-var timeLeft = 60;
+window.onload = function () {
 
-// functions
 
-function timer() {
-    var timeInterval = setInterval(function () {
-        timeLeft--;
-        timerEl.textContent = timeLeft;
-        if (timeLeft === 0) {
-            clearInterval(timeInterval);
+    // var hideElements = document.getElementById("question-container").style.display = "none";
+
+    // Timer variables
+    var timerEl = document.getElementById("countdowntimer");
+    var timeLeft = 5;
+
+
+    // quiz variables 
+    var questionDisplay = document.getElementById("question-display");
+    var ansBtn = Array.from(document.getElementsByClassName("ans-btn"));
+    var questionIndex = 0;
+    var score = 0;
+    var questionCounter = 0;
+    var acceptingAnswers = true;
+    var currentQuestion = {};
+    var availableQuestions = [];
+
+    var questionBank = [
+        {
+            "question": "what is a duck?",
+            "answer0": "bird",
+            "answer1": "dog",
+            "answer2": "cat",
+            "answer3": "fish",
+            "correctAnswer": "bird"
+        },
+        {
+            "question": "how much is a dozen?",
+            "answer0": "12",
+            "answer1": "1",
+            "answer2": "6",
+            "answer3": "24",
+            "correctAnswer": "answer1"
+        },
+        {
+            "question": "what planet is closest to the sun?",
+            "answer0": "Earth",
+            "answer1": "Mercury",
+            "answer2": "Saturn",
+            "answer3": "Uranus",
+            "correctAnswer": "answer2"
+        },
+        {
+            "question": "what currency is the Dong from?",
+            "answer0": "Thailand",
+            "answer1": "Australia",
+            "answer2": "Philippines",
+            "answer3": "Vietnam",
+            "correctAnswer": "answer4"
+        },
+        {
+            "question": "How many great lakes are there?",
+            "answer0": "1",
+            "answer1": "12",
+            "answer2": "4",
+            "answer3": "5",
+            "correctAnswer": "answer4"
+        },
+    ];
+
+    // const variables 
+    const questionPoints = 1;
+    const questionMax = 4;
+    // console.log(ansBtn)
+
+    function myFunction() {
+
+
+
+        $("#start-btn").on("click", startgame)
+
+
+
+        function startGame() {
+
+
+            var timeInterval = setInterval(function () {
+                timeLeft--;
+                timerEl.textContent = timeLeft;
+                if (timeLeft === 0) {
+                    clearInterval(timeInterval);
+                }
+            }, 1000);
+
+            questionCounter = 0;
+            score = 0;
+            availableQuestions = [...questionBank];
+            console.log(availableQuestions)
+            getNewQuestion();
 
         }
-    }, 1000);
-}
 
-timer()
 
-// quiz variables 
-var questionDisplay = document.getElementById("question-display");
-var ansBtn = Array.from(document.getElementsByClassName("ans-btn"));
-var questionIndex = 0;
-var score = 0;
-var questionCounter = 0;
-// var answerChoice = true;
-var currentQuestion = {};
-var availableQuestions = [];
 
-var questionBankObj = [
-    {
-        "question": "what is a duck?",
-        "answer1": "bird",
-        "answer2": "dog",
-        "answer3": "cat",
-        "answer4": "fish",
-        "correctAnswer": "answer1"
-    },
-    {
-        "question": "how much is a dozen?",
-        "answer1": "12",
-        "answer2": "1",
-        "answer3": "6",
-        "answer4": "24",
-        "correctAnswer": "answer1"
-    },
-    {
-        "question": "what planet is closest to the sun?",
-        "answer1": "Earth",
-        "answer2": "Mercury",
-        "answer3": "Saturn",
-        "answer4": "Uranus",
-        "correctAnswer": "answer2"
-    },
-    {
-        "question": "what currency is the Dong from?",
-        "answer1": "Thailand",
-        "answer2": "Australia",
-        "answer3": "Philippines",
-        "answer4": "Vietnam",
-        "correctAnswer": "answer4"
-    },
-    {
-        "question": "How many great lakes are there?",
-        "answer1": "1",
-        "answer2": "12",
-        "answer3": "4",
-        "answer4": "5",
-        "correctAnswer": "answer4"
-    },
-];
+        startGame();
 
-console.log(ansBtn)
-// start game 
-// var startBtn = document.getElementById("start-btn");
-// startBtn = addEventListener("click", startGame);
+        function getNewQuestion() {
+            questionCounter++;
+            questionIndex = Math.floor(Math.random() * availableQuestions.length);
+            currentQuestion = availableQuestions[questionIndex];
+            questionDisplay.innerText = currentQuestion.question;
 
-function startGame() {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questionBankObj];
-    console.log(availableQuestions)
-    getNewQuestion();
-};
+            for (var i = 0; i < ansBtn.length; i++) {
+                ansBtn[i].innerText = questionBank[questionIndex]["answer" + i];
+            }
 
-startGame();
+            // ansBtn.forEach(answer => {
+            //     var number = answer.dataset["number"];
+            //     answer.innerText = currentQuestion["answer" + number];
 
-function getNewQuestion() {
-    questionCounter++;
-    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    questionDisplay.innerText = currentQuestion.questionDisplay;
-    console.log(questionDisplay);
-    console.log(currentQuestion);
-};
+            // });
 
-// getNewQuestion();
+        };
 
 
 
 
-// function showQuestion() {
-//     var questionDisplay = document.getElementById("question-display");
-//     var answerDisplayOne = document.getElementById("answer1");
-//     var answerDisplayTwo = document.getElementById("answer2");
-//     var answerDisplayThree = document.getElementById("answer3");
-//     var answerDisplayFour = document.getElementById("answer4");
 
-//     questionDisplay.textContent = questionBankObj[questionIndex].question;
-//     answerDisplayOne.textContent = questionBankObj[questionIndex].answer1;
-//     answerDisplayTwo.textContent = questionBankObj[questionIndex].answer2;
-//     answerDisplayThree.textContent = questionBankObj[questionIndex].answer3;
-//     answerDisplayFour.textContent = questionBankObj[questionIndex].answer4;
 
-// };
+        // function showQuestion() {
+        //     var questionDisplay = document.getElementById("question-display");
+        //     var answerDisplayOne = document.getElementById("answer1");
+        //     var answerDisplayTwo = document.getElementById("answer2");
+        //     var answerDisplayThree = document.getElementById("answer3");
+        //     var answerDisplayFour = document.getElementById("answer4");
 
-// showQuestion();
+        //     questionDisplay.textContent = questionBankObj[questionIndex].question;
+        //     answerDisplayOne.textContent = questionBankObj[questionIndex].answer1;
+        //     answerDisplayTwo.textContent = questionBankObj[questionIndex].answer2;
+        //     answerDisplayThree.textContent = questionBankObj[questionIndex].answer3;
+        //     answerDisplayFour.textContent = questionBankObj[questionIndex].answer4;
+
+        // };
+
+        // showQuestion();
+
+    };
